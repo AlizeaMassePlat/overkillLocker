2e8a69404c5df1b7c2b43f8c4abe7414322270da
AlizeaMassePlat <alizea.masse@gmail.com>
2024-07-18 10:20:41 +0200

    init mobile app ion component

--- mobile-app/capacitor.config.ts
+++ mobile-app/capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'ionic-app-base',
+  appId: 'ionic.package',
+  appName: 'package',
   webDir: 'dist'
 };
 
