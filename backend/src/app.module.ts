d56c82115dff98beeb5a405af7ddb0899cf6ca3e
jeremy <jeremy.nowak@laplateforme.io>
2024-07-18 15:39:44 +0200

    deleted connection database, added entity conection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,22 +1,14 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 import { UserModule } from './user/user.module';
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT, 10),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      // synchronize: true,
-      autoLoadEntities: true,
-    }),
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
     UserModule,
   ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
