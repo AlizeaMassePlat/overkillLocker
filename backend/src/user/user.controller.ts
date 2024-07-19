2eb756bdfcdbaceaefa7f162413f6de9333b9260
jeremy <jeremy.nowak@laplateforme.io>
2024-07-19 16:10:57 +0200

    Modification of the type of the "id"  and rename the route "create" as "register"

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { UserService } from './user.service';
+
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
@@ -15,27 +16,26 @@ export class UsersController {
     return this.usersService.findAll();
   }
 
-  @Post('/create')
+  @Post('/register')
   async controllerCreate(@Body() createUserDto: CreateUserDto ) {
-    
     const salt =  await genSalt();
     createUserDto.password =  await hash(createUserDto.password, salt);
-    return this.usersService.create(createUserDto);
+    return this.usersService.register(createUserDto);
   }
 
   @Get('/finduserbyid/:id')
-  async controllerFindOne(@Param('id') id: number): Promise<User> {
-    return await this.usersService.findOne(+id);
+  async controllerFindOne(@Param('id') id: string): Promise<User> {
+    return await this.usersService.findOne(id);
   }
 
   @Patch('/update/:id')
   async controllerUpdate(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return await this.usersService.update(+id, updateUserDto);
+    return await this.usersService.update(id, updateUserDto);
   }
 
   @Delete('/delete/:id')
   async controllerRemove(@Param('id') id: string) {
-    return await this.usersService.remove(+id);
+    return await this.usersService.remove(id);
   }
   
   @Post('/login')
