e9fbf931578724cab3fef77a4a5502940766a89b
jeremy <jeremy.nowak@laplateforme.io>
2024-07-19 16:08:26 +0200

    Changed type of the "id" from number to "string" because we've chosen to use UUID

--- backend/src/user/dto/login-user.dto.ts
+++ backend/src/user/dto/login-user.dto.ts
@@ -1,10 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
-import { IsNumber } from 'class-validator';
+import { IsNumber, IsString } from 'class-validator';
 
 
 export class LoginUserDto extends PartialType(CreateUserDto) {
 
-    @IsNumber()
-    id: number;
+    @IsString()
+    id: string;
 }
