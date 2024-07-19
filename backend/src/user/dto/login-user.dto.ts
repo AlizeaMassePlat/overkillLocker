bbd9fbbe22cc9723074a5f850360130f0c36333f
jeremy <jeremy.nowak@laplateforme.io>
2024-07-19 17:50:11 +0200

    Deleting useless import

--- backend/src/user/dto/login-user.dto.ts
+++ backend/src/user/dto/login-user.dto.ts
@@ -1,6 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
-import { IsNumber, IsString } from 'class-validator';
+import { IsString } from 'class-validator';
 
 
 export class LoginUserDto extends PartialType(CreateUserDto) {
