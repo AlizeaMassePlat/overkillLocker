726b260aabb41b1bfc8ec458d96dd1efe2dfd871
jeremy <jeremy.nowak@laplateforme.io>
2024-07-19 17:49:19 +0200

    Defining my dto fields

--- backend/src/card/dto/create-card.dto.ts
+++ backend/src/card/dto/create-card.dto.ts
@@ -1,1 +1,17 @@
-export class CreateCardDto {}
+import { IsString, IsEmail, IsNumber, Length, IsOptional, IsBoolean, IsDate, IsDefined  } from 'class-validator';
+export class CreateCardDto {
+
+
+
+    @IsDefined()
+    @IsString()
+    card_identifier: string;
+
+    @IsDefined()
+    @IsNumber()
+    state: number;
+
+    @IsDefined()
+    @IsBoolean()
+    is_deleted: boolean;
+}
