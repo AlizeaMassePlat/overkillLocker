846e9b39d19a818b2dd1b7a1a994f2d03146f408
corentin-roussel <corentin.roussel@laplateforme.io>
2024-07-22 10:22:59 +0200

    add Erromodul to appmodule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,11 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { UserModule } from './user/user.module';
+import { ErrorModule } from './error/error.module';
+
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
     UserModule,
+    ErrorModule,
   ],
   controllers: [],
   providers: [],
