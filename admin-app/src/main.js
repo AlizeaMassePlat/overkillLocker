f37d08a0c0bcbfcb263b3b7f19d1a9ef18abf4c0
Your Name <sofiane.ziri@laplateforme.io>
2024-07-22 11:14:18 +0200

    composant bouton V1

--- admin-app/src/main.js
+++
@@ -1,9 +0,0 @@
-// src/main.js
-import { createApp } from 'vue';
-import App from './views/App.vue';
-import router from './router';
-import "./style/variables.css";
-const app = createApp(App);
-
-app.use(router);
-app.mount('#app');
