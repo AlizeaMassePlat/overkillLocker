3c106abb740440abedd6611dfba121c955f5af35
jeremy <jeremy.nowak@laplateforme.io>
2024-07-22 12:17:02 +0200

    Setting up table reservation

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,6 +6,7 @@ import { CardModule } from './card/card.module';
 import { ErrorModule } from './error/error.module';
 import { LogModule } from './log/log.module';
 import { LockerModule } from './locker/locker.module';
+import { ReservationModule } from './reservation/reservation.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { LockerModule } from './locker/locker.module';
     ErrorModule,
     LogModule,
     LockerModule,
+    ReservationModule,
   ],
   controllers: [],
   providers: [],
