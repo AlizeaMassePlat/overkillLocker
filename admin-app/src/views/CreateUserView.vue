8a23c2f519b7c2a7d325b40a54b26b0317067a3d
Guangquan Ye <shinz@MacBook-Pro-de-Guangquan.local>
2024-07-23 17:09:30 +0200

    merge dev into over-93 et minor fix

--- admin-app/src/views/CreateUserView.vue
+++ admin-app/src/views/CreateUserView.vue
@@ -15,7 +15,6 @@ export default {
     UserFormComponent,
   }
 };
-
 </script>
 
 <style scoped>
@@ -23,28 +22,5 @@ export default {
   display: flex;
   flex-direction: row;
   width: 100%;
-}
-/* Add your styles here */
-.users-view {
-  display: flex;
-}
-
-.content {
-  flex-grow: 1;
-  padding: 20px;
-}
-
-.back-button {
-  background-color: #ffa62b;
-  color: white;
-  border: none;
-  border-radius: 4px;
-  padding: 10px 20px;
-  cursor: pointer;
-  margin-bottom: 20px;
-}
-
-.back-button:hover {
-  background-color: #e59426;
 }
 </style>
