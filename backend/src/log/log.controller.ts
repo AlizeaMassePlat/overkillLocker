2c167540ea944db675809265ff529b4091d92936
corentin-roussel <corentin.roussel@laplateforme.io>
2024-07-23 16:08:13 +0200

    create all route adn controller

--- backend/src/log/log.controller.ts
+++ backend/src/log/log.controller.ts
@@ -2,33 +2,44 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { LogService } from './log.service';
 import { CreateLogDto } from './dto/create-log.dto';
 import { UpdateLogDto } from './dto/update-log.dto';
+import { deleteLogDto } from './dto/delete-log.dto';
 
 @Controller('log')
 export class LogController {
   constructor(private readonly logService: LogService) {}
 
-  @Post()
-  create(@Body() createLogDto: CreateLogDto) {
+  @Post("/create")
+  async createLog(@Body() createLogDto: CreateLogDto) {
     return this.logService.create(createLogDto);
   }
 
   @Get()
-  findAll() {
+  async findLogs() {
     return this.logService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.logService.findOne(+id);
+  async findLog(@Param('id') id_log: number) {
+    return this.logService.findOne(id_log);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateLogDto: UpdateLogDto) {
-    return this.logService.update(+id, updateLogDto);
+  @Get('/locker/:id')
+  async findLogsByLocker(@Param('id') id: number) {
+    return this.logService.findLogsByLocker(id);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.logService.remove(+id);
+  @Get('/group_locker/:id')
+  async findLogsByGroupLocker(@Param('id') id_group_locker: number) {
+    return this.logService.findLogsByGroupLocker(id_group_locker);
+  }
+
+  @Patch('/update/:id')
+  async updateLogById(@Param('id') id_log: number, @Body() updateLogDto: UpdateLogDto) {
+    return this.logService.updateLogById(+id_log, updateLogDto);
+  }
+
+  @Delete('/delete/:id')
+  async deleteLogById(@Param('id') id_log: number) {
+    return this.logService.deleteLogById(+id_log);
   }
 }
