8cea7a38a595525484c4e52f5605c98ba5033c23
corentin-roussel <corentin.roussel@laplateforme.io>
2024-07-25 00:45:14 +0200

    modify dto upgrad to add isoptional to every field

--- backend/src/group_locker/dto/update-group_locker.dto.ts
+++ backend/src/group_locker/dto/update-group_locker.dto.ts
@@ -1,23 +1,29 @@
-import { IsBoolean, IsInt, IsLatLong, IsNumber, IsString } from 'class-validator';
+import { IsBoolean, IsInt, IsLatLong, IsNumber, IsOptional, IsString } from 'class-validator';
 import { Point } from 'typeorm';
 
 export class UpdateGroupLockerDto {
    
+    @IsOptional()
     @IsLatLong()
     coordinate?:string;
 
+    @IsOptional()
     @IsInt()
     state?:number;
 
+    @IsOptional()
     @IsString()
     locker_type?:string;
 
+    @IsOptional()
     @IsInt()
     locker_count?:number;
 
+    @IsOptional()
     @IsString()
     name_place?:string;
 
+    @IsOptional()
     @IsBoolean()
     is_delete?:boolean;
 }
