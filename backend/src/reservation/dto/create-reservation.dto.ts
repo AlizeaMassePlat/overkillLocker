59e9c91cc07c61407800000119477f3f62ac22a3
jeremy <jeremy.nowak@laplateforme.io>
2024-07-25 01:24:32 +0200

    dto creation

--- backend/src/reservation/dto/create-reservation.dto.ts
+++ backend/src/reservation/dto/create-reservation.dto.ts
@@ -1,1 +1,25 @@
-export class CreateReservationDto {}
+import { IsInt, IsBoolean, IsEnum, IsOptional, IsNumber } from 'class-validator';
+import { Reserve_type } from '../entities/reservation.entity'; 
+export class CreateReservationDto {
+
+    @IsOptional()
+    @IsNumber()
+    id?: number;
+  
+
+    @IsInt()
+    state: number;
+
+    @IsEnum(Reserve_type)
+    reserve_type: Reserve_type;
+
+    @IsBoolean()
+    is_delete: boolean;
+
+    @IsInt()
+    userId: number;  
+
+    @IsInt()
+    lockerId: number; 
+
+}
--- backend/src/reservation/dto/update-reservation.dto.ts
+++ backend/src/reservation/dto/update-reservation.dto.ts
@@ -1,4 +1,29 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateReservationDto } from './create-reservation.dto';
+import { IsInt, IsBoolean, IsEnum, IsOptional, IsNumber } from 'class-validator';
+import { Reserve_type } from '../entities/reservation.entity'; 
 
-export class UpdateReservationDto extends PartialType(CreateReservationDto) {}
+export class UpdateReservationDto {
+
+    @IsOptional()
+    @IsNumber()
+    id?: number;
+
+    @IsOptional()
+    @IsInt()
+    state?: number;
+
+    @IsOptional()
+    @IsEnum(Reserve_type)
+    reserve_type?: Reserve_type;
+
+    @IsOptional()
+    @IsBoolean()
+    is_delete?: boolean;
+
+    @IsOptional()
+    @IsInt()
+    userId?: number;  
+
+    @IsOptional()
+    @IsInt()
+    lockerId?: number; 
+}
